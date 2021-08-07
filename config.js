c1fe3b769fb70ad3ce54c3795eda4d51c0caf099
trumpbaba2 <54684913+trumpbaba2@users.noreply.github.com>
2021-08-07 20:44:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-OwnerID: [""],    
+OwnerID: ["471402387840434187"],    
 GuildID: "",
-VoiceID: "",
-Token: "",
-Prefix: ["!", "."],
-URL: "mongo",
+VoiceID: "873621966945402952",
+Token: "ODczNjIxODQwMzIzNTcxNzQz.YQ7FvQ.-PGrgObiSb7Pxcwj1VaHlOTsakU",
+Prefix: [".", "/"],
+URL: "https://discord.gg/y6KMbqCWZm",
 
-Tag: "tagınız",
+Tag: "✾",
 SecondaryTag: "•",
 
 ServerMessage: "Striga ❤️ Striga",
